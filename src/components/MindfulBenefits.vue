8a947e9c9dc66c2225418cb272df66b7cb5b7346
kribow21 <kristininnotech@gmail.com>
2021-11-11 21:41:01 -0700

    working on benefits page content

---
+++ src/components/MindfulBenefits.vue
@@ -0,0 +1,21 @@
+<template>
+    <article>
+        <a href="https://www.helpguide.org/harvard/benefits-of-mindfulness.htm" target="_blank"><h1>Benefits of Mindfulness</h1></a>
+        <h3>Practices for Improving Emotional and Physical Well-Being</h3>
+        <p><b>Mindfulness improves well-being</b> increasing your capacity for mindfulness supports many attitudes that contribute to a satisfied life. Being mindful makes it easier to savor the pleasures in life as they occur, helps you become fully engaged in activities, and creates a greater capacity to deal with adverse events. By focusing on the here and now, many people who practice mindfulness find that they are less likely to get caught up in worries about the future or regrets over the past, are less preoccupied with concerns about success and self-esteem, and are better able to form deep connections with others.</p>
+        <p><b>Mindfulness improves physical health</b> if greater well-being isn’t enough of an incentive, scientists have discovered that mindfulness techniques help improve physical health in a number of ways. Mindfulness can: help relieve stress, treat heart disease, lower blood pressure, reduce chronic pain, , improve sleep, and alleviate gastrointestinal difficulties.</p>
+        <p><b>Mindfulness improves mental health</b> in recent years, psychotherapists have turned to mindfulness meditation as an important element in the treatment of a number of problems, including: depression, substance abuse, eating disorders, couples’ conflicts, anxiety disorders, and obsessive-compulsive disorder.</p>
+    </article>
+</template>
+
+<script>
+    export default {
+        name : "MindfulBenefits"
+    }
+</script>
+
+<style scoped>
+article{
+    background-image: linear-gradient(to bottom left, #86e9f7, #FFECB3);
+}
+</style>
--- src/views/Benefits.vue
+++ src/views/Benefits.vue
@@ -1,18 +1,21 @@
 <template>
     <div>
         <NavBar2/>
+        <MindfulBenefits/>
         <Footer/>
     </div>
 </template>
 
 <script>
 import Footer from '../components/Footer.vue'
+import MindfulBenefits from '../components/MindfulBenefits.vue'
 import NavBar2 from '../components/NavBar2.vue'
     export default {
         name : "Benefits",
         components: {
             NavBar2,
-                Footer,
+            MindfulBenefits,
+            Footer,
         },
     }
 </script>
